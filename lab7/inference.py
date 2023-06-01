0c8e12a1c8a225cc7a670c5e00ba40dc29959c15
kirilllzaitsev <kzaitsev88@gmail.com>
2023-06-01 17:24:55 +0200

    inference working with time.sleep(2)

--- lab7/inference.py
+++ lab7/inference.py
@@ -22,16 +22,145 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from PIL import Image, ImageTk
 
+
+def draw_gui(classes, correct_count, test_len, x, class_idx, pred):
+    root = ctk.CTk()
+    root.title("Real-time inference")
+    imgplot = plt.imshow(x.astype(np.uint8))
+    f = Figure()
+    a = f.add_subplot(111)
+    a.imshow(x.astype(np.uint8))
+    # store figure to file
+    f.savefig("test.png")
+    image = Image.open("test.png")
+    # photo = ImageTk.PhotoImage(image)
+    photo = ctk.CTkImage(image, size=(200, 200))
+    root.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+    root.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
+    color = "#0B8457" if (np.argmax(pred) == class_idx) else "#F24C4C"
+    darkgrey = "#2D2D2D"
+    yellow = "#FFC107"
+    quit = "#DA0037"
+    next = "#3E497A"
+    next2 = "#3AB4F2"
+    ctk.CTkButton(master=root, image=photo, text=None).grid(
+        row=0, column=0, padx=(10, 5), pady=10, columnspan=2
+    )
+    # pre label should be on the left of the grid
+    text_var = tk.StringVar(value=f"Prediction: {classes[np.argmax(pred)]}")
+    pred_label = ctk.CTkButton(
+        root,
+        textvariable=text_var,
+        font=("Helvetica", 18, "bold"),
+        fg_color=color,
+        text_color="white",
+        hover=False,
+    )
+    pred_label.grid(row=1, column=0, padx=(10, 5), pady=10, columnspan=1, sticky="nsew")
+    target_label = ctk.CTkButton(
+        root,
+        text=f"Target: {classes[class_idx]}",
+        font=("Helvetica", 18, "bold"),
+        fg_color=color,
+        text_color="white",
+        hover=False,
+    )
+    target_label.grid(
+        row=1, column=1, padx=(5, 10), pady=10, columnspan=1, sticky="nsew"
+    )
+    accuracy = (correct_count / test_len) * 100
+    text_var = tk.StringVar(value=f"Accuracy: {accuracy:.2f}%")
+    accuracy_label = ctk.CTkButton(
+        root,
+        textvariable=text_var,
+        font=("Helvetica", 18, "bold"),
+        fg_color=yellow,
+        text_color=darkgrey,
+        hover=False,
+    )
+    accuracy_label.grid(
+        row=2, column=0, padx=(10, 10), pady=10, columnspan=2, sticky="nsew"
+    )
+    # add button to go to the next image
+    ctk.CTkButton(
+        master=root,
+        text="Next",
+        command=root.destroy,
+        fg_color=next,
+        font=("Helvetica", 18, "bold"),
+        hover_color=next2,
+    ).grid(row=3, column=0, padx=(10, 5), pady=10, columnspan=2, sticky="nsew")
+    ctk.CTkButton(
+        master=root,
+        text="Quit",
+        command=sys.exit,
+        fg_color=next,
+        hover_color=quit,
+        font=("Helvetica", 18, "bold"),
+    ).grid(row=4, column=0, padx=(10, 5), pady=10, columnspan=2, sticky="nsew")
+
+    root.mainloop()
+
+
+def main(x_test, y_test, classes, ser: serial.Serial):
+    correct_count = 0
+    # define how many images from the test set to send to the MCU
+    test_len = 2
+    img_size = (28, 28)
+    # img_size = (64,64)
+    num_pixels = np.product(img_size)
+
+    for x, y in zip(x_test[:test_len], y_test[:test_len]):
+        x = cv2.resize(x, img_size)
+        class_idx = y
+        ser.write(x.tobytes())
+        ser.flush()
+        # time.sleep(1)
+        img = ser.read(num_pixels)
+        img = np.frombuffer(img, dtype=np.uint8)
+        assert len(img) == num_pixels, f"Expected {num_pixels} bytes, got {len(img)}"
+        # time.sleep(1)
+        pred = ser.read(10)
+        pred = np.frombuffer(pred, dtype=np.uint8)
+        print(pred)
+        print(
+            f"Target: {classes[class_idx]}, Prediction (from MCU): {classes[np.argmax(pred)]}"
+        )
+        if np.argmax(pred) == class_idx:
+            correct_count += 1
+
+        # use_gui = True
+        use_gui = False
+        if use_gui:
+            draw_gui(
+                classes=classes,
+                correct_count=correct_count,
+                test_len=test_len,
+                x=x,
+                class_idx=class_idx,
+                pred=pred,
+            )
+        time.sleep(2)
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+    print(f"Accuracy: {(correct_count / test_len) * 100:.2f}%")
+
+
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ds_name", type=str, required=False, default="fmnist")
+    args, _ = parser.parse_known_args()
+    print(args)
+
     x_test = np.load("x_test_fmnist.npy")
     y_test = np.load("y_test_fmnist.npy").squeeze()
 
     ctk.set_appearance_mode("dark")
     # ctk.set_default_color_theme("dark-blue")
 
-    sys.argv = ["", "fmnist"]
-
-    dataset_name = sys.argv[1]
+    dataset_name = args.ds_name
     classes = []
 
     if dataset_name == "mnist":
@@ -62,6 +191,8 @@ if __name__ == "__main__":
             "Bag",
             "Ankle boot",
         ]
+    else:
+        raise ValueError("Invalid dataset name")
 
     print(f"Loaded x with shape: {x_test.shape}")
     print(f"Loaded y with shape: {y_test.shape}")
@@ -72,108 +203,9 @@ if __name__ == "__main__":
     ser.flush()
     ser.flushInput()
     ser.flushOutput()
-
-    correct_count = 0
-    # define how many images from the test set to send to the MCU
-    test_len = 20
-    # img_size = (28,28)
-    img_size = (64,64)
-    num_pixels = np.product(img_size)
-
-    for x, y in zip(x_test[:test_len], y_test[:test_len]):
-        x = cv2.resize(x, img_size)
-        class_idx = y
-        ser.write(x.tobytes())
-        time.sleep(1)
-        img = ser.read(num_pixels)
-        img = np.frombuffer(img, dtype=np.uint8)
-        print("Image sent to the MCUs: \n {}".format(img))
-        assert len(img) == num_pixels, f"img: {img}"
-        time.sleep(1)
-        pred = ser.read(10)
-        pred = np.frombuffer(pred, dtype=np.uint8)
-        print(pred)
-        print(
-            f"Target: {classes[class_idx]}, Prediction (from MCU): {classes[np.argmax(pred)]}"
-        )
-        if np.argmax(pred) == class_idx:
-            correct_count += 1
-
-        root = ctk.CTk()
-        root.title("Real-time inference")
-        imgplot = plt.imshow(x.astype(np.uint8))
-        f = Figure()
-        a = f.add_subplot(111)
-        a.imshow(x.astype(np.uint8))
-        # store figure to file
-        f.savefig("test.png")
-        image = Image.open("test.png")
-        # photo = ImageTk.PhotoImage(image)
-        photo = ctk.CTkImage(image)
-        root.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
-        root.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
-        color = "#0B8457" if (np.argmax(pred) == class_idx) else "#F24C4C"
-        darkgrey = "#2D2D2D"
-        yellow = "#FFC107"
-        quit = "#DA0037"
-        next = "#3E497A"
-        next2 = "#3AB4F2"
-        ctk.CTkButton(master=root, image=photo, text=None).grid(
-            row=0, column=0, padx=(10, 5), pady=10, columnspan=2
-        )
-        # pre label should be on the left of the grid
-        text_var = tk.StringVar(value=f"Prediction: {classes[np.argmax(pred)]}")
-        pred_label = ctk.CTkButton(
-            root,
-            textvariable=text_var,
-            font=("Helvetica", 18, "bold"),
-            fg_color=color,
-            text_color="white",
-            hover=False,
-        )
-        pred_label.grid(
-            row=1, column=0, padx=(10, 5), pady=10, columnspan=1, sticky="nsew"
-        )
-        target_label = ctk.CTkButton(
-            root,
-            text=f"Target: {classes[class_idx]}",
-            font=("Helvetica", 18, "bold"),
-            fg_color=color,
-            text_color="white",
-            hover=False,
-        )
-        target_label.grid(
-            row=1, column=1, padx=(5, 10), pady=10, columnspan=1, sticky="nsew"
-        )
-        accuracy = (correct_count / test_len) * 100
-        text_var = tk.StringVar(value=f"Accuracy: {accuracy:.2f}%")
-        accuracy_label = ctk.CTkButton(
-            root,
-            textvariable=text_var,
-            font=("Helvetica", 18, "bold"),
-            fg_color=yellow,
-            text_color=darkgrey,
-            hover=False,
-        )
-        accuracy_label.grid(
-            row=2, column=0, padx=(10, 10), pady=10, columnspan=2, sticky="nsew"
-        )
-        # add button to go to the next image
-        ctk.CTkButton(
-            master=root,
-            text="Next",
-            command=root.destroy,
-            fg_color=next,
-            font=("Helvetica", 18, "bold"),
-            hover_color=next2,
-        ).grid(row=3, column=0, padx=(10, 5), pady=10, columnspan=2, sticky="nsew")
-        ctk.CTkButton(
-            master=root,
-            text="Quit",
-            command=sys.exit,
-            fg_color=next,
-            hover_color=quit,
-            font=("Helvetica", 18, "bold"),
-        ).grid(row=4, column=0, padx=(10, 5), pady=10, columnspan=2, sticky="nsew")
-
-        root.mainloop()
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    try:
+        main(x_test, y_test, classes, ser)
+    finally:
+        ser.close()

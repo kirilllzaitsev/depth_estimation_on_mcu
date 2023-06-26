58257517a370a687e17972dfc7d951c32e1e0efc
kirilllzaitsev <kzaitsev88@gmail.com>
2023-06-26 22:29:13 +0200

    rm unused

--- stm32_code/metrics.py
+++
@@ -1,5 +0,0 @@
-def calc_metrics(fp_model, test_images, test_labels):
-    # Evaluate the model on the test set
-    fp_test_loss, fp_test_acc = fp_model.evaluate(test_images, test_labels, verbose=2)
-    print("Test accuracy:", fp_test_acc)
-    print("Test loss:", fp_test_loss)
